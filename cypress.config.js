21288c19a9b25e893c464090fbe7978a0f841383
AndreDG88 <andre.soares.designer@gmail.com>
2025-01-16 17:12:48 -0300

    adição e configuração do mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,13 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  // abaixo é a configuração do mochawesome
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/relatorio', // para definir o diretório de armazenamento
+    overwrite: true, // para salvar por cima de relatórios antigos
+    html: true,
+    json: false,
+    timestamp: 'ddmmyyyy_HHMMss' // definir formato de data e hora do documento
+  }
 });
